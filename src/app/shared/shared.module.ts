2f64fb470d8e1d57bc034b69fca68fa4305c61d1
Daniel Pineda <danielpineda.profe@gmail.com>
2023-11-28 16:34:26 -0500

    Rutas funcionando

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from '../app-routing.module';
 
-
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AboutPageComponent,
+    HomePageComponent,
+    SidebarComponent,
+    ContactPageComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
+  ],
+  exports:[
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent,
   ]
 })
 export class SharedModule { }
